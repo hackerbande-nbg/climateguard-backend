02055dfee898e10cab0046dbf2acb05a71570c6f
codingkrabbe <mirringandreas@gmail.com>
2025-05-31 16:00:23 +0200

    feat: fix migration errors

--- project/migrations/env.py
+++ project/migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlmodel import SQLModel                       # NEW
-
+import os
 from alembic import context
 
 import app.models                       # NEW
@@ -13,6 +13,10 @@ import app.models                       # NEW
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+
+config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
